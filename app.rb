85aa3fd8abd3276f5880bfaf801b7b255baf2f3a
paosch <paola_schiaffino@yahoo.co.uk>
2018-03-13 15:21:08 +0000

    Setting up a testing environment

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require './lib/link'
 
 class BookmarkManager < Sinatra::Base
 get '/' do
-
+#p ENV # not concerned with the web app but with Sinatra settings. It prints variable in Terminal after I do rackup config.ru
   # @links = [
   #   "https://www.google.co.uk",
   #   "http://www.bbc.co.uk/news",
--- lib/link.rb
+++ lib/link.rb
@@ -1,8 +1,6 @@
 require 'pg'
 
 class Link
-
-
   def self.all
 
     # [
@@ -10,7 +8,12 @@ class Link
     #   "http://www.bbc.co.uk/news",
     #   "http://www.pitchero.com/clubs/dulwichhamlet"
     # ]
-    connection = PG.connect :dbname => 'bookmark_manager', :user => 'paosch'
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
     result = connection.exec "SELECT * FROM links"
     result.map { |link| link['url'] }
   end
